df4ab06b2191c78f18640da3a906c0699498f42b
root <andyb@datadevil.co.uk>
2019-09-02 20:52:27 +0100

    Added outlines for major message types

--- chronicle-consumer/src/consumer.py
+++ chronicle-consumer/src/consumer.py
@@ -40,6 +40,47 @@ async def handler(websocket, path):
 				block_num = int(data['block_num'])
 				await websocket.send(str(block_num))
 				logger.info(f"Block {block_num} acknowledged!")
+#				logger.info(j)
+#				logger.info('')
+
+		elif msg[:21] == '{"msgtype":"TX_TRACE"':
+			j = json.loads(msg)
+			data = j['data']
+			block_num = int(data['block_num'])
+			trace = data['trace']
+			tx_id = trace['id']
+			cpu_usage_us = trace['cpu_usage_us']
+			net_usage_words = trace['net_usage_words']
+			action_traces = trace['action_traces']
+			for action_trace in action_traces:
+				receiver = action_trace['receiver']
+				act = action_trace['act']
+				account = act['account']
+				name = act['name']
+				auth = act['authorization']
+				data = act['data']
+#				logger.info(f'{receiver} - {account} - {name} - {data}')
+
+		elif msg[:20] == '{"msgtype":"TBL_ROW"':
+			j = json.loads(msg)
+
+		elif msg[:20] == '{"msgtype":"ABI_UPD"':
+			j = json.loads(msg)
+
+		elif msg[:24] == '{"msgtype":"ENCODER_ERR"':
+			j = json.loads(msg)
+
+		elif msg[:28] == '{"msgtype":"BLOCK_COMPLETED"':
+			j = json.loads(msg)
+
+		elif msg[:17] == '{"msgtype":"FORK"':
+			j = json.loads(msg)
+
+		else:
+			j = json.loads(msg)
+			logger.info(j)
+			logger.info('')
+
 
 
 start_server = websockets.serve(handler, '0.0.0.0', 8800)
