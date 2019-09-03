461746c20274861a4e2f108c9ff8bcb4241d00a5
root <andyb@datadevil.co.uk>
2019-09-03 11:34:11 +0100

    Improved config options

--- chronicle-consumer/src/storage.py
+++ chronicle-consumer/src/storage.py
@@ -8,7 +8,6 @@ from datetime import datetime
 from debug_log import logger
 
 ARCHIVE_MODE = os.getenv('ARCHIVE_MODE', '')
-ARCHIVE_BLOCKS_PER_FILE = int(os.getenv('ARCHIVE_BLOCKS_PER_FILE', '100'))
 
 class ArchiveStorage():
 	def __init__(self, mode=ARCHIVE_MODE, block_per_file=ARCHIVE_BLOCKS_PER_FILE):
